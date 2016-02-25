a03fe0fae99a722f3d7e99dfc38fec4bc373b3fe
Tom Burnell <thrburnell@gmail.com>
2016-02-25 19:26:58 +0000

    Added IR builder for predicate logic formulas.

---
+++ src/main/java/com/ipandora/core/FormulaBuildingVisitor.java
@@ -0,0 +1,149 @@
+package com.ipandora.core;
+
+import com.ipandora.api.predicate.formula.*;
+import com.ipandora.api.predicate.term.Constant;
+import com.ipandora.api.predicate.term.Term;
+import com.ipandora.api.predicate.term.Variable;
+import com.ipandora.parser.PredicateLogicBaseVisitor;
+import com.ipandora.parser.PredicateLogicLexer;
+import com.ipandora.parser.PredicateLogicParser;
+import org.antlr.v4.runtime.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FormulaBuildingVisitor extends PredicateLogicBaseVisitor<Formula> {
+
+    @Override
+    public Formula visitFormula(PredicateLogicParser.FormulaContext ctx) {
+
+        PredicateLogicParser.IffElementContext lhs = ctx.lhs;
+        PredicateLogicParser.IffElementContext rhs = ctx.rhs;
+
+        Formula lhsFormula = visit(lhs);
+
+        if (rhs == null) {
+            return lhsFormula;
+        }
+
+        Formula rhsFormula = visit(rhs);
+        return new IffFormula(lhsFormula, rhsFormula);
+    }
+
+    @Override
+    public Formula visitIffElement(PredicateLogicParser.IffElementContext ctx) {
+
+        PredicateLogicParser.ImpliesElementContext lhs = ctx.lhs;
+        PredicateLogicParser.ImpliesElementContext rhs = ctx.rhs;
+
+        Formula lhsFormula = visit(lhs);
+
+        if (rhs == null) {
+            return lhsFormula;
+        }
+
+        Formula rhsFormula = visit(rhs);
+        return new ImpliesFormula(lhsFormula, rhsFormula);
+    }
+
+    @Override
+    public Formula visitImpliesElement(PredicateLogicParser.ImpliesElementContext ctx) {
+
+        PredicateLogicParser.ConjunctionContext lhs = ctx.lhs;
+        PredicateLogicParser.ConjunctionContext rhs = ctx.rhs;
+
+        Formula lhsFormula = visit(lhs);
+
+        if (rhs == null) {
+            return lhsFormula;
+        }
+
+        Formula rhsFormula = visit(rhs);
+        return new OrFormula(lhsFormula, rhsFormula);
+    }
+
+    @Override
+    public Formula visitConjunction(PredicateLogicParser.ConjunctionContext ctx) {
+
+        PredicateLogicParser.NegElementContext lhs = ctx.lhs;
+        PredicateLogicParser.NegElementContext rhs = ctx.rhs;
+
+        Formula lhsFormula = visit(lhs);
+
+        if (rhs == null) {
+            return lhsFormula;
+        }
+
+
+        Formula rhsFormula = visit(rhs);
+        return new AndFormula(lhsFormula, rhsFormula);
+    }
+
+    @Override
+    public Formula visitNegElement(PredicateLogicParser.NegElementContext ctx) {
+
+        Formula formula = visit(ctx.elem);
+
+        if (ctx.not != null) {
+            return new NotFormula(formula);
+        }
+
+        return formula;
+    }
+
+    @Override
+    public Formula visitElement(PredicateLogicParser.ElementContext ctx) {
+
+        if (ctx.quant != null) {
+            return visit(ctx.quant);
+        }
+
+        if (ctx.pred != null) {
+            return visit(ctx.pred);
+        }
+
+        // An element is either quant, pred or form
+        return visit(ctx.form);
+    }
+
+    @Override
+    public Formula visitQuantified(PredicateLogicParser.QuantifiedContext ctx) {
+
+        String variableName = ctx.var.getText();
+        Variable variable = new Variable(variableName);
+
+        Formula formula = visit(ctx.elem);
+
+        if (ctx.quant.getType() == PredicateLogicLexer.FORALL) {
+            return new ForallFormula(variable, formula);
+        } else {
+            return new ExistsFormula(variable, formula);
+        }
+    }
+
+    @Override
+    public Formula visitPredicate(PredicateLogicParser.PredicateContext ctx) {
+
+        String predicateName = ctx.name.getText();
+
+        List<Term> params = new ArrayList<Term>();
+        for (PredicateLogicParser.ArgContext arg : ctx.args.args) {
+            params.add(generateTerm(arg));
+        }
+
+        return new PredicateFormula(predicateName, params);
+    }
+
+    private Term generateTerm(PredicateLogicParser.ArgContext ctx) {
+
+        Token var = ctx.var;
+        Token constant = ctx.constant;
+
+        if (var != null) {
+            return new Variable(var.getText());
+        } else {
+            return new Constant(constant.getText());
+        }
+    }
+
+}
