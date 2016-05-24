3dd1484cb257fed5bd58e54ac60c269f44e1cbd5
Tom Burnell <thrburnell@gmail.com>
2016-05-24 12:13:54 +0100

    Added function creators test utils

---
+++ src/test/java/com/ipandora/testutils/FunctionCreators.java
@@ -0,0 +1,28 @@
+package com.ipandora.testutils;
+
+import com.ipandora.api.predicate.formula.Formula;
+import com.ipandora.api.predicate.function.*;
+import com.ipandora.api.predicate.term.Term;
+import com.ipandora.api.predicate.term.Variable;
+
+import java.util.List;
+
+public class FunctionCreators {
+
+    public static MathematicalFunction mathFun(String name, List<Variable> args, List<FunctionCase> cases) {
+        return new MathematicalFunction(name, args, cases);
+    }
+
+    public static FunctionCase funCase(Term expr, FunctionCaseCondition cond) {
+        return new FunctionCase(expr, cond);
+    }
+
+    public static IfCondition ifCond(Formula form) {
+        return new IfCondition(form);
+    }
+
+    public static OtherwiseCondition otherCond() {
+        return new OtherwiseCondition();
+    }
+
+}
