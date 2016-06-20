3c344b86ff7fee4b4e5973c39b729531788b73ef
Tom Burnell <thrburnell@gmail.com>
2016-06-20 20:45:10 +0100

    Added left padding to ind proof lines

--- src/main/web/app/js/components/BaseCase/FunctionDefinitionLine.jsx
+++ src/main/web/app/js/components/BaseCase/FunctionDefinitionLine.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FunctionDefinitionLine = ({ body }) => (
   <tr>
-    <td className="col-sm-7">= { body }</td>
+    <td className="col-sm-7 left-padding">= { body }</td>
     <td className="col-sm-5">by function def.</td>
   </tr>
 )
--- src/main/web/app/js/components/InductiveCase/ArithmeticLine.jsx
+++ src/main/web/app/js/components/InductiveCase/ArithmeticLine.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ArithmeticLine = ({ body }) => (
   <tr>
-    <td className="col-sm-7">= { body }</td>
+    <td className="col-sm-7 left-padding">= { body }</td>
     <td className="col-sm-5">by arithmetic</td>
   </tr>
 )
